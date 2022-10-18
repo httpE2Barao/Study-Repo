0a4e16cff6070d0e500925c559afb42c4f2f7b96
httpE2Barao <elias.barao@hotmail.com>
2022-10-18 17:07:10 -0300

    .

--- JavaScript/C. Condição/Aninhada.js
+++ JavaScript/C. Condição/Aninhada.js
@@ -9,3 +9,17 @@ if (idade < 16) {
     } else {
     console.log('Voto Obrigatório')}
 }
+
+
+var agora = new Date()
+var hora = agora.getHours()
+if (hora >= 6 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora > 12 && hora <= 18){
+    console.log('Boa tarde!')
+} else if (hora > 18 && hora <= 23) {
+    console.log('Boa noite!')
+} else {
+    console.log('Boa madrugada!')
+}
+console.log(`Agora são ${hora} horas.`)
