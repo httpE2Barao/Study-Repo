c6dd20eefb73222f9bdaa0c501e43291cf7b5f06
httpE2Barao <elias.barao@hotmail.com>
2022-10-18 16:57:30 -0300

    .

---
+++ JavaScript/C. Condição/SimplesComposta.js
@@ -0,0 +1,16 @@
+// Condição simples
+var vel = 69
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Velocidade máxima ultrapassada. Você foi multado!')
+}
+console.log('Dirija com cinto de segurança ;)')
+
+//Condição Composta
+var país = 'Brasil'
+console.log('Vivendo em ${país}')
+if (país != 'Brasil') {
+    console.log('Você é estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
