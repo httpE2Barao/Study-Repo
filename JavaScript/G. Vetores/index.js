2a58183e3dedb8c5e91ed62206f6d131a0b447f3
httpE2Barao <elias.barao@hotmail.com>
2022-10-20 16:06:56 -0300

    .

---
+++ JavaScript/G. Vetores/index.js
@@ -0,0 +1,29 @@
+let valores = [8, 1, 7, 4, 2, 9]
+/*
+var num = [5,8,4]
+
+num[3] = 9
+num.push(6)
+num.sort()
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+
+
+        //Usando for
+for(let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+        //Simplificado
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+        //indexOf()
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
+
