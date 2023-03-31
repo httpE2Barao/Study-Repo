8bb89e37735aa989e987cb92f7fcaf4cba47bf7e
Elias E. Barão <e2barao@hotmail.com>
2023-03-30 23:00:41 -0300

    Update negociacoes-services.ts

--- TypeScript/Negociações/app/src/services/negociacoes-services.ts
+++ TypeScript/Negociações/app/src/services/negociacoes-services.ts
@@ -3,7 +3,7 @@ import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacoesServices {
     public obterNegociacaoDoDia(): Promise<Negociacao[]> {
-        return fetch('http://localhost:8080/dados')
+        return fetch('http://3.133.118.212:8080/dados')
         .then(res => res.json())
         .then((dados: NegociacoesDoDia[]) => {
             return dados.map(dado => {
