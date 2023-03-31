23d38ece46f50447a83194542333b22cae6f6090
Elias E. Barão <e2barao@hotmail.com>
2023-03-30 23:00:58 -0300

    Update negociacoes-services.js

--- TypeScript/Negociações/app/dist/js/services/negociacoes-services.js
+++ TypeScript/Negociações/app/dist/js/services/negociacoes-services.js
@@ -1,7 +1,7 @@
 import { Negociacao } from "../models/negociacao.js";
 export class NegociacoesServices {
     obterNegociacaoDoDia() {
-        return fetch('http://localhost:8080/dados')
+        return fetch('http://3.133.118.212:8080/dados')
             .then(res => res.json())
             .then((dados) => {
             return dados.map(dado => {
