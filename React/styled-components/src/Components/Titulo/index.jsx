9f4284a5bf035ae5b4e38e15022ccaba2a33914b
httpE2Barao <elias.barao@hotmail.com>
2023-04-12 12:32:08 -0300

    refatorando em styled-components

--- React/styled-components/src/Components/Titulo/index.jsx
+++ React/styled-components/src/Components/Titulo/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import Styled from "styled-components";
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
+const Titulo = Styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
 export default Titulo;
