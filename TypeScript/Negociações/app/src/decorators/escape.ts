a1bbc46fd4733cff3a7095b6b4d7e3405b38b6ea
httpE2Barao <elias.barao@hotmail.com>
2023-05-24 15:22:33 -0300

    revisando código

--- TypeScript/Negociações/app/src/decorators/escape.ts
+++ TypeScript/Negociações/app/src/decorators/escape.ts
@@ -1,19 +1,19 @@
-export function escape (
-    target:any,
+export function escape(
+    target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
 ) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args: any[]) {
         let retorno = metodoOriginal.apply(this, args);
-        /* if (typeof retorno === 'string') {
+        if (typeof retorno === 'string') {
             console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`);
             retorno = retorno
                 .replace(/<script>[\s\S]*?<\/script>/, '');
-        }*/
-        return retorno;
-    }   
+
+            return retorno;
+        }
 
 
-    return descriptor;
-}
+        return descriptor;
+    }
